52220b3f197afe312fbe3b713b82ad28bd0a39bf
Fabian <Fabian@FabiPC>
2013-04-01 11:34:27 +0200

    Project created Basic structure added

---
+++ picsuGUI/src/org/jackl/serial/SerialCommunicator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jackl.serial;
+
+/**
+ *
+ * @author Fabian
+ */
+public class SerialCommunicator {
+    
+}
